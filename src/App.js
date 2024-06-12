8fe0b49a38f04388200c59c1bf6bbfcb7998d40c
Cadfemm <anusha.s@cadfem.in>
2024-06-12 11:56:48 +0530

    msg99

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Cyberdyne from "./pages/Cyberdyne";
 import treatments from "./pages/treatments";
 import pablotests from "./pages/pablotests";
 import Tymo from "./pages/Tymo";
-import Plabo from "./pages/Plabo";
 import Output from "./pages/Output";
 import Signup from "./pages/Signup";
 import SpinalInjury from "./pages/SpinalInjury";
@@ -34,10 +32,9 @@ function App() {
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
           <Route path="/treatment/Subtreatments" exact component={treatments} />
-          <Route path="/treatment/Cyberdyne" exact component={Cyberdyne} />
           <Route path="/treatment/pablotests" exact component={pablotests} />
           <Route path="/treatment/Tymo" exact component={Tymo} />
-          <Route path="/treatment/Plabo" exact component={Plabo} />
+        
           <Route path="/Output" exact component={Output} />
           <Route path="/" exact component={Login} />
           <Route path="/Signup" exact component={Signup} />
